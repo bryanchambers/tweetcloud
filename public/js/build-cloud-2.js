fe1d3182f85c134c0058707769461142f6c655cb
Bryan <bryches@BrychesT420.Ubuntu>
2017-02-16 20:19:49 -0500

    Prevent words from going outside canvas bounds

--- public/js/build-cloud-2.js
+++ public/js/build-cloud-2.js
@@ -13,9 +13,8 @@ function buildCloud2(wordcount) {
 	canvas.height = 1000;
 	canvas.style.width = '1000px';
 	canvas.style.height = '500px';
-
 	draw = canvas.getContext('2d');
-
+	
 
 	
 	for(var i = 0; i < wordcount.length; i++) {
@@ -29,8 +28,17 @@ function buildCloud2(wordcount) {
 		draw.fillStyle = RGBtoHEX(color);
 
 		draw.font = fontsize + 'px Arial';
-		var x = Math.random()*1800 + 50;
-		var y = Math.random()*900 + 50;
+		var x = Math.random()*2000;
+		var y = Math.random()*1000;
+
+		if(y < fontsize) { y = fontsize; }
+		if(y > 950) { y = 950; }
+
+		var width = draw.measureText(word).width;
+		var xmax = 2000 - width - 10;
+		while(x > xmax) { x = Math.random()*2000; }
+
+
 		draw.fillText(word, x, y);
 	}
 
